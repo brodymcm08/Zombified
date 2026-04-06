8dee41a2e92064534bce73a1b25748525eaea93d
brodymcm <bmanlax13@gmail.com>
2026-04-05 22:08:33 -0400

    Initial mod setup.

---
+++ src/client/java/brodymcm/zombified/client/ZombifiedClient.java
@@ -0,0 +1,10 @@
+package brodymcm.zombified.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZombifiedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
