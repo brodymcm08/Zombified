92e54eb54ba8009c83795c2a7be00311842fb15c
brodymcm <bmanlax13@gmail.com>
2026-04-05 22:15:20 -0400

    Initial mod setup

---
+++ src/main/java/brodymcm/zombified/mixin/ZombieReinforcementMixin.java
@@ -0,0 +1,28 @@
+package brodymcm.zombified.mixin;
+
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.EntitySpawnReason;
+import net.minecraft.world.entity.SpawnGroupData;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.level.ServerLevelAccessor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Zombie.class)
+public class ZombieReinforcementMixin
+{
+    @Inject(method = "finalizeSpawn(Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/DifficultyInstance;Lnet/minecraft/world/entity/EntitySpawnReason;Lnet/minecraft/world/entity/SpawnGroupData;)Lnet/minecraft/world/entity/SpawnGroupData;",
+            at = @At("TAIL"))
+    private void boostReinforcements(ServerLevelAccessor serverLevelAccessor, DifficultyInstance difficultyInstance, EntitySpawnReason entitySpawnReason, SpawnGroupData spawnGroupData, CallbackInfoReturnable ci)
+    {
+        Zombie self = (Zombie)(Object)this;
+        if(self.getAttribute(Attributes.SPAWN_REINFORCEMENTS_CHANCE) != null)
+        {
+            self.getAttribute(Attributes.SPAWN_REINFORCEMENTS_CHANCE).setBaseValue(1.0);
+            System.out.println("Reinforcement chance boosted!");
+        }
+    }
+}
