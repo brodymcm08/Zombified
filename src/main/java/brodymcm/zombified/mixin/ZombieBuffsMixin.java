3d32f992416b7d85a4c05fb8e65e6e2a79a7fd87
brodymcm <bmanlax13@gmail.com>
2026-04-09 21:44:25 -0400

    Initial mod setup.

---
+++ src/main/java/brodymcm/zombified/mixin/ZombieBuffsMixin.java
@@ -0,0 +1,11 @@
+package brodymcm.zombified.mixin;
+
+
+import net.minecraft.world.entity.monster.Zombie;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Zombie.class)
+public class ZombieBuffsMixin
+{
+
+}
